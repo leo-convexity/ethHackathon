2c53401499201eefe4d6f43005f20f0a62d4ec05
Kristofer Peterson <kris@tranception.com>
2021-05-29 13:21:42 +0100

    Add common.py

---
+++ py/common.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env pythoan3
+import decimal
+
+def D(x: int, decimals: int = 0):
+    '''Convert integer to scaled decimal'''
+    y = decimal.Decimal(x)
+    y /= 10**decimals
+    return y
