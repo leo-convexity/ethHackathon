eeaf0e320734916a645c890589fbfeb6f79b0842
Kristofer Peterson <kris@tranception.com>
2021-05-29 17:36:35 +0100

    Rename get_local_signers.py to local_account_balances.py Add local_account_uniswap_transact.py to buy/sell tokens from uniswap Add more helper functions etc to common.py

--- py/common.py
+++ py/common.py
@@ -5,6 +5,10 @@ import web3
 from collections import namedtuple
 from typing import Optional, Tuple
 
+# These Uniswap deployment addresses are the same for mainnet, ropsten, rinkeby, goerli and kovan
+UNIVERSAL_UNISWAP_FACTORY_ADDRESS = web3.main.to_checksum_address('0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f')
+UNIVERSAL_UNISWAP_ROUTER_ADDRESS = web3.main.to_checksum_address('0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D')
+
 def D(x: int, decimals: int = 0):
     '''Convert integer to scaled decimal'''
     y = decimal.Decimal(x)
@@ -18,11 +22,26 @@ def new_web3(url: str) -> web3.Web3:
     return w3
 
 def get_contract_definitions(w3: web3.Web3, chain: str = 'mainnet'):
+    assert chain in ('mainnet', 'ropsten', 'rinkeby', 'goerli', 'kovan'), f'unsupported chain: {chain}'
+
     cfgs = json.load(open(f'../compound-config/networks/{chain}.json'))
     abis = json.load(open(f'../compound-config/networks/{chain}-abi.json'))
 
     contract_cUSDC = w3.eth.contract(address=cfgs['cTokens']['cUSDC']['address'], abi=abis['cUSDC'])
     contract_USDC = w3.eth.contract(address=cfgs['cTokens']['cUSDC']['underlying'], abi=abis['USDC'])
+
+    # uniswap factory
+    abi = json.load(open('../uniswap-v2-core-1.0.1-abi/UniswapV2Factory.json'))['abi']
+    contract_uniswap_v2_factory = w3.eth.contract(address=UNIVERSAL_UNISWAP_FACTORY_ADDRESS, abi=abi)
+
+    # uniswap router
+    abi = json.load(open('../uniswap-v2-periphery@1.1.0-beta.0-abi/UniswapV2Router02.json'))['abi']
+    contract_uniswap_v2_router = w3.eth.contract(address=UNIVERSAL_UNISWAP_ROUTER_ADDRESS, abi=abi)
+
+    # WETH (from uniswap router)
+    abi = json.load(open('../uniswap-v2-periphery@1.1.0-beta.0-abi/WETH9.json'))['abi']
+    address = contract_uniswap_v2_router.functions.WETH().call()
+    contract_WETH = w3.eth.contract(address=address, abi=abi)
 
     # sanity checks
     assert contract_USDC.functions.symbol().call() == 'USDC'
@@ -30,21 +49,51 @@ def get_contract_definitions(w3: web3.Web3, chain: str = 'mainnet'):
     assert cfgs['cTokens']['cUSDC']['decimals'] == 8
     assert contract_cUSDC.functions.symbol().call() == 'cUSDC'
     assert contract_cUSDC.functions.decimals().call() == 8
+    assert contract_WETH.functions.symbol().call() == 'WETH'
+    assert contract_WETH.functions.decimals().call() == 18
 
     return {
         'USDC': contract_USDC,
         'cUSDC': contract_cUSDC,
+        'WETH': contract_WETH,
+        'UniswapV2Factory': contract_uniswap_v2_factory,
+        'UniswapV2Router02': contract_uniswap_v2_router,
     }
 
 def get_token(w3: web3.Web3, token_name: str, contract_definitions: Optional[dict] = None) -> Tuple:
     if contract_definitions is None:
         contract_definitions = get_contract_definitions(w3)
     token_contract = contract_definitions[token_name]
-    token_type = namedtuple(token_name, 'contract decimals balanceOf')
-    token_decimals=token_contract.functions.decimals().call()
+    token_type = namedtuple(token_name, 'contract symbol decimals balanceOf')
+    token_symbol = token_contract.functions.symbol().call()
+    token_decimals = token_contract.functions.decimals().call()
     token = token_type(
         contract=token_contract,
+        symbol=token_symbol,
         decimals=token_decimals,
-        balanceOf=lambda address: D(token_contract.functions.balanceOf(address).call(), token_decimals)
+        balanceOf=lambda address: D(token_contract.functions.balanceOf(address).call(), token_decimals),
     )
     return token
+
+def get_uniswap_factory(w3: web3.Web3, contract_definitions: Optional[dict] = None) -> Tuple:
+    if contract_definitions is None:
+        contract_definitions = get_contract_definitions(w3)
+    name = 'UniswapV2Factory'
+    contract_definition = contract_definitions[name]
+    contract_type = namedtuple(name, 'contract')
+    contract = contract_type(
+        contract=contract_definition,
+    )
+    return contract
+
+def get_uniswap_router(w3: web3.Web3, contract_definitions: Optional[dict] = None) -> Tuple:
+    if contract_definitions is None:
+        contract_definitions = get_contract_definitions(w3)
+    name = 'UniswapV2Router02'
+    contract_definition = contract_definitions[name]
+    contract_type = namedtuple(name, 'contract WETH')
+    contract = contract_type(
+        contract=contract_definition,
+        WETH=lambda: contract_definition.functions.WETH().call(),
+    )
+    return contract
---
+++ py/local_account_uniswap_transact.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+'''
+Iterates and print accounts on the Ethereum node.
+For a local node (forked or otherwise) there should be 20 accounts with 10,000 ETH each.
+'''
+import json
+import decimal
+import web3
+import time
+import sys
+from collections import namedtuple
+from pprint import pprint
+from common import D, new_web3, get_contract_definitions, get_token, get_uniswap_router
+
+def usage():
+    print(f'usage: {sys.argv[0]} [account] [buy|sell] [quantity] [token]')
+    exit(1)
+
+try:
+    arg_act, arg_op, arg_qty, arg_token = sys.argv[1:]
+except:
+    usage()
+
+w3 = new_web3('http://127.0.0.1:8545')
+
+contract_definitions = get_contract_definitions(w3, 'mainnet')
+USDC = get_token(w3, 'USDC', contract_definitions)
+cUSDC = get_token(w3, 'cUSDC', contract_definitions)
+WETH = get_token(w3, 'WETH', contract_definitions)
+UNISWAP = get_uniswap_router(w3, contract_definitions)
+
+try:
+    index = int(arg_act)
+    account = w3.eth.accounts[index]
+    quantity = decimal.Decimal(arg_qty)
+    assert quantity > 0
+
+    if arg_op.lower() == 'sell':
+        side = -1
+    elif arg_op.lower() == 'buy':
+        side = +1
+    else:
+        raise ValueError(f'invalid operation: {arg_op}')
+
+    token = {'usdc': USDC, 'cusdc': cUSDC, 'weth': WETH}[arg_token.lower()]
+except Exception as exc:
+    print(exc)
+    usage()
+
+block_number = w3.eth.block_number
+
+print()
+print('# Ethereum')
+print(f'{"block_number":<24} {block_number:,d}')
+print()
+print(f'{"#":<2}    {"Account":<42}    {"ETH":>24}    {"WETH":>24}    {"USDC":>24}    {"cUSDC":>24}')
+eth_balance = w3.eth.get_balance(account)
+weth_balance = WETH.balanceOf(account)
+usdc_balance = USDC.balanceOf(account)
+cusdc_balance = cUSDC.balanceOf(account)
+print(f'{index:<2}    {account:<42}    {D(eth_balance, 18):>24.18f}    {weth_balance:>24.18f}    {usdc_balance:>24.6f}    {cusdc_balance:>24.8f}')
+print()
+
+# Scale quantity for token's expected number of decimals
+quantity *= 10**token.decimals
+adj_quantity = int(quantity)
+assert adj_quantity == quantity, f'token {token.symbol} has only {token.decimals} decimal(s)'
+
+# Deadline for uniswap transaction completion or else it will revert when committed
+deadline = int(time.time()) + 30
+
+if side > 0:
+    max_spend = max(0, eth_balance - 10**18) # Save 1 ETH
+    value = max_spend
+    function = UNISWAP.contract.functions.swapETHForExactTokens(adj_quantity, [WETH.contract.address, token.contract.address], account, deadline)
+elif side < 0:
+    min_receive = 0
+    value = 0
+    function = UNISWAP.contract.functions.swapExactTokensForETH(adj_quantity, min_receive, [token.contract.address, WETH.contract.address], account, deadline)
+
+transaction_template = {'from': account, 'value': value}
+result = function.call(transaction_template)
+print(f'transaction call result: {result}')
+
+print('proceed (y/n) ?')
+reply = input()
+if reply.strip().lower() == 'y':
+    transaction_hash = function.transact(transaction_template)
+    print(f'transaction hash: {transaction_hash}')
+    result = w3.eth.wait_for_transaction_receipt(transaction_hash, timeout=120, poll_latency=0.1)
+
+    # This sort of makes the transaction receipt more readable
+    adj_result = dict(result)
+    adj_result['logs'] = list(map(dict, adj_result['logs']))
+    pprint(adj_result)
